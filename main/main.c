77ad4f84a467b43d7f232aea0d1fa7d78065a50e
Eduardo Menezes <drummeduardo@gmail.com>
2024-08-02 23:14:17 -0300

    Mapa/Camera
    Redimensionamento do mapa e implementação da camera de personagem

--- main/main.c
+++ main/main.c
@@ -7,11 +7,20 @@ int main() {
 	posicaoY = LARGURA / 2 - LADO;
 	Mapa_t mapa;
 	Texturas_t texturas;
-  texturas.personagemPrincipal = LoadTexture("./texturas/sprite.png"); 
+  	texturas.personagemPrincipal = LoadTexture("./texturas/sprite.png"); 
 
 
 	SetTargetFPS(60);
 	SetExitKey(KEY_BACKSPACE);
+	LoadMap(&mapa);
+	ToggleFullscreen();   
+
+	Vector2 position = { posicaoX, posicaoY };
+	Camera2D camera = { 0 };
+    camera.target = position;
+    camera.offset = (Vector2){ LARGURA/2.0f, ALTURA/2.0f };
+    camera.rotation = 0.0f;
+    camera.zoom = 4.0f;
 
 	while (!WindowShouldClose() && programa_rodando) {
 		if (menu_inicial_rodando) {
@@ -22,15 +31,25 @@ int main() {
 			if (IsKeyPressed(KEY_ESCAPE)) {
 				menu_inicial_rodando = true;
 			}
-			LoadMap(&mapa);
+
+			//if (camera.target.x < LARGURA / 2 / camera.zoom) camera.target.x = LARGURA / 2 / camera.zoom;
+        	//if (camera.target.y < ALTURA / 2 / camera.zoom) camera.target.y = ALTURA / 2 / camera.zoom;
+        	//if (camera.target.x > LARGURA / 2 / camera.zoom) camera.target.x = LARGURA / 2 / camera.zoom;
+        	//if (camera.target.y > ALTURA / 2 / camera.zoom) camera.target.y = ALTURA / 2 / camera.zoom;
+
+			BeginMode2D(camera);
+
 			DrawMap(&mapa);
+
 			ClearBackground(RAYWHITE);
 			Vector2 position = { posicaoX, posicaoY };
+			camera.target = position;
 
 			//DrawTextureEx(texturas.personagemPrincipal, position, 0.0f, 3.0f, WHITE);
 			DrawTexture(texturas.personagemPrincipal, posicaoX, posicaoY, WHITE);
 			//DrawRectangle(posicaoX, posicaoY, LADO, LADO, GREEN);
 			EndDrawing();
+			EndMode2D();
 		}
 	}
 
